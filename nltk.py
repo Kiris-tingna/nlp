57529846c85c22f232fd4829aa93b9e8f280a06f
luolitao <luolitao@gmail.com>
2017-03-09 18:19:00 +0800

    2

--- nltk.py
+++ nltk.py
@@ -5,10 +5,4 @@
 @time:2017/3/8 0008 12:19
 """
 import nltk
-from urllib3 import urlopen
 
-
-url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
-html = urlopen(url).read()
-raw = nltk.clean_html(html)
-print(raw)
