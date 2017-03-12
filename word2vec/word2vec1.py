9a852dd6f51c2185c0949fc4171bdf67c42c28c3
luolitao <luolitao@gmail.com>
2017-03-12 18:16:11 +0800

    new

---
+++ word2vec/word2vec1.py
@@ -0,0 +1,29 @@
+from gensim.models import Word2Vec, LdaModel
+from gensim import corpora
+
+
+class MyCorpus(object):
+    def __init__(self, docs):
+        self.docs = docs
+
+    def __iter__(self):
+        for line in self.docs:
+            yield line.split()
+
+
+if __name__ == "__main__":
+    documents = ["Human machine interface for lab abc computer applications",
+                 "A survey of user opinion of computer system response time",
+                 "The EPS user interface management system",
+                 "System and human system engineering testing of EPS",
+                 "Relation of user perceived response time to error measurement",
+                 "The generation of random binary unordered trees",
+                 "The intersection graph of paths in trees",
+                 "Graph minors IV Widths of trees and well quasi ordering", "Graph minors A survey"]
+
+    corpuss = MyCorpus(documents)
+
+    model = Word2Vec(corpuss, size=10, window=2, min_count=0)
+    print    (model["Human"])
+    print    (model.similarity('Human', 'system'))
+    print(model.similarity('Human', 'user'))
