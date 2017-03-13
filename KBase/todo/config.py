74f7da780d21b42fa0dcfee279451753a8db9475
luolitao <luolitao@gmail.com>
2017-03-14 00:16:16 +0800

    use sqlite

--- KBase/todo/config.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-# @author:Eric Luo
-# @file:config.py
-# @time:2017/3/13 0013 12:51
--- KBase/todo/manage.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-# @author:Eric Luo
-# @file:manage.py
-# @time:2017/3/13 0013 12:27
