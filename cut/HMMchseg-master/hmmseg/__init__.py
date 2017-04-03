650b09a67bc3c3df7d8d118a3cce18e9809b384b
luolitao <luolitao@gmail.com>
2017-04-03 19:26:03 +0800

    170403

---
+++ cut/HMMchseg-master/hmmseg/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+'''
+	Hidden Markov Model for Chinese sentence segmentation
+	@author: Yigoss.Panyi
+	@date: 2014-6-7
+'''
+from viterbi import gen_seg_viterbi
+import re
+
+def cut(sentence):
+	'''
+	Call the viterbi Algorithm and do the segmentation,cache the result in a cache dictionary file
+	Parameters:
+		sentence: The String to be segmented
+	'''
+	unlog_word_cache = open("dict/dict.txt.temp", "w")
+	if not( type(sentence) is unicode):
+		try:
+			sentence = sentence.decode('utf-8')
+		except:
+			sentence = sentence.decode("gbk", 'ignore')
+	# sentence
+	re_han = re.compile(ur"([\u4E00-\u9FA5a-zA-Z0-9+#&\._]+)", re.U)
+	re_skip = re.compile(ur"(\r\n|\s)", re.U)
+	re_eng = re.compile(ur"([a-zA-Z0-9+#&\._]+)", re.U)
+	blocks = re_han.split(sentence)
+	resultls = []
+	# result = ""
+	for statement in blocks:
+		if(len(statement) == 0):
+			continue
+		if(re_han.match(statement)):
+			eng_blocks = re_eng.split(statement)
+			for blk in eng_blocks:
+				if(len(blk) == 0):
+					continue
+				if(re_eng.match(blk)):
+					# result += "/" + blk
+					resultls.append(blk)
+				else:
+					seg_result = gen_seg_viterbi(blk)
+					resultls.extend(seg_result)
+					# result += "/".join(seg_result)
+		else:
+			# result += "/" + statement + "/"
+			resultls.append(statement)
+	# print resultls
+	for w in resultls:
+		if(len(w) > 1):
+			unlog_word_cache.write("%s %d %s\n" %(w, 5, 'uk'))
+	unlog_word_cache.close()
+	# return result
+	return "/".join(resultls)
